6f9046d88f47661e0d59ceb0149ffc03d4e4c516
Pedro Regenes <pedroregenes@MacBook-Air-de-Pedro.local>
2023-05-10 01:23:39 -0300

    fix: edit name page and edit local css select

--- frontend/src/components/selectArticles/index.js
+++ frontend/src/components/selectArticles/index.js
@@ -1,19 +1,19 @@
-// import "./styles.css";
+import "./styles.css";
 
 function SelectArticles() {
 
   return (
-    <div className="home__box-contact">
-      <div className="home__box-info bg1">
-        <h3 className="home__box-h3">Plantas para começar sua Urban Jungle.</h3>
+    <div className="select__box-contact">
+      <div className="select__box-info bg1">
+        <h3 className="select__box-h3">Plantas para começar sua Urban Jungle.</h3>
       </div>
-      <div className="home__box-info bg2">
-        <h3 className="home__box-h3">
-          Faça do seu Home Office mais agradável.
+      <div className="select__box-info bg2">
+        <h3 className="select__box-h3">
+          Faça do seu select Office mais agradável.
         </h3>
       </div>
-      <div className="home__box-info bg3">
-        <h3 className="home__box-h3">Plantas para cultivar na água!</h3>
+      <div className="select__box-info bg3">
+        <h3 className="select__box-h3">Plantas para cultivar na água!</h3>
       </div>
     </div>
   );
